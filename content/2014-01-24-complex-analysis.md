30da04f71c12a1ac6f1ee9bd9dc05bf47667f345
fehiepsi <fehiepsi@gmail.com>
2014-01-25 13:56:16 +0900

    add new post

---
+++ content/2014-01-24-complex-analysis.md
@@ -0,0 +1,7 @@
+Title: Solutions to Rudin, Real and Complex Analysis
+Date: 2014-01-24 17:21
+Slug: complex-analysis
+
+This is my solution:
+<!-- PELICAN_END_SUMMARY -->
+{% notebook complex-analysis.ipynb cells[2:] %}
--- publishconf.py
+++ publishconf.py
@@ -16,9 +16,11 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
+
+OUTPUT_RETENTION = [".git"]
